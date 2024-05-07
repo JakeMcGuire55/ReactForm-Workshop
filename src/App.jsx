ed582ff61ece0ec9b466b3c46c340dda66fa9ec8
JakeMcGuire55 <jakemmcguire@gmail.com>
2024-05-06 21:44:35 -0400

    Workshop Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Authenticate from './components/Authenticate'
-import SignUpForm from './components/SignUpForm'
+import { useState } from "react";
+import "./App.css";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   const [token, setToken] = useState(null);
 
   return (
     <>
-    <SignUpForm token={token} setToken={setToken} />
-    <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
